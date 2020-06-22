a5d1e6ee51f5afb91ec009f1d19818eefe87b104
eric.r.driggs <eric.r.driggs@hulu.com>
2020-06-22 07:35:01 -0700

    move

---
+++ src/main/java/com/ericdriggs/ragnarok/xml/junit/Error.java
@@ -0,0 +1,26 @@
+package com.ericdriggs.ragnarok.xml.junit;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "value"
+})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(builderMethodName = "builderForError")
+@Data
+public class Error {
+
+    @XmlValue
+    protected String value;
+    @XmlAttribute(name = "message")
+    protected String message;
+    @XmlAttribute(name = "type", required = true)
+    protected String type;
+}
---
+++ src/main/java/com/ericdriggs/ragnarok/xml/surefire/Skipped.java
@@ -0,0 +1,24 @@
+package com.ericdriggs.ragnarok.xml.surefire;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "value"
+})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(builderMethodName = "builderForSkipped")
+@Data
+public class Skipped {
+
+    @XmlValue
+    protected String value;
+    @XmlAttribute(name = "message")
+    protected String message;
+}
