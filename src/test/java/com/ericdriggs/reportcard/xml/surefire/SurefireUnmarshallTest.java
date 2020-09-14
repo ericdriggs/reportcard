bf3b7fc548618cc123b626099ea1926d9e7f4d31
eric.r.driggs <eric.r.driggs@hulu.com>
2020-09-14 11:46:07 -0700

    wip surefire

--- src/test/java/com/ericdriggs/reportcard/xml/surefire/SurefireUnmarshallTest.java
+++ src/test/java/com/ericdriggs/reportcard/xml/surefire/SurefireUnmarshallTest.java
@@ -11,6 +11,7 @@ import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class SurefireUnmarshallTest {
@@ -28,8 +29,11 @@ public class SurefireUnmarshallTest {
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             Testsuite testsuite = (Testsuite) jaxbUnmarshaller.unmarshal(new StringReader(xmlString));
             System.out.println(testsuite);
-//            assertEquals(3, testsuite.tests);
-            //TODO: add assertions
+            assertEquals(3, testsuite.tests);
+            assertEquals(3, testsuite.testcase.size());
+            assertNotNull(testsuite.testcase.get(0).getSkipped());
+            assertNotNull(testsuite.testcase.get(1).getFailure());
+            assertNotNull(testsuite.testcase.get(2).getError());
         } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
