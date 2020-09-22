53af6fa16817898ed6f2e89dbe4904c2e733dc53
eric.r.driggs <eric.r.driggs@hulu.com>
2020-09-21 22:20:59 -0700

    wip reportcard scanner

---
+++ reportcard-scanner/src/main/java/com/ericdriggs/reportcard/reportcardscanner/ReportcardScannerApplication.java
@@ -0,0 +1,13 @@
+package com.ericdriggs.reportcard.reportcardscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportcardScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportcardScannerApplication.class, args);
+	}
+
+}
---
+++ reportcard-scanner/src/test/java/com/ericdriggs/reportcard/reportcardscanner/ReportcardScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericdriggs.reportcard.reportcardscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportcardScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
