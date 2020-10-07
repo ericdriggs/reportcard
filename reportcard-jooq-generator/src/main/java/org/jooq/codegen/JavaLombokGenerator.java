e405ef5e6e4e457cfc9362f7b9b084c515342f78
eric.r.driggs <eric.r.driggs@hulu.com>
2020-10-07 00:28:07 -0700

    line wrap

--- reportcard-jooq-generator/src/main/java/org/jooq/codegen/JavaLombokGenerator.java
+++ reportcard-jooq-generator/src/main/java/org/jooq/codegen/JavaLombokGenerator.java
@@ -4,12 +4,15 @@ import org.jooq.meta.CatalogDefinition;
 import org.jooq.meta.SchemaDefinition;
 
 /**
- * Uses lombok.Generated as a class annootation instead of (javax.annotation.processing.Generated or javax.annotation.Generated) because
+ * Uses lombok.Generated as a class annootation instead of a javax.annotation.(processing?.)Generated
  * <ol>
- *     <li>javax.annotation.Generated only has <code>SOURCE</code> retention, while lombok.Generated <code>CLASS</code> retention. </li>
+ *     <li>javax.annotation.(processing?.)Generated only has <code>SOURCE</code> retention,
+ *         while lombok.Generated <code>CLASS</code> retention. </li>
  *     <li>lombok.Generated is ignored by jacoco when calculating code coverage</li>
  * </ol>
- * Note: lombok is not a dependency of jooq, so if you use this generator, it is YOUR responsibility to ensure your project supports the
+ *
+ * Note: lombok is not a dependency of jooq, so if you use this generator,
+ * it is YOUR responsibility to ensure your project supports the
  * lombok.Generated annotation
  *
  * @see <a href="https://github.com/jOOQ/jOOQ/issues/8617">https://github.com/jOOQ/jOOQ/issues/8617</a>
@@ -18,13 +21,15 @@ import org.jooq.meta.SchemaDefinition;
 @SuppressWarnings("unused")
 public class JavaLombokGenerator extends JavaGenerator {
     @Override
-    protected void printClassAnnotations(JavaWriter out, SchemaDefinition schema, CatalogDefinition catalog) {
+    protected void printClassAnnotations(JavaWriter out, SchemaDefinition schema,
+                                         CatalogDefinition catalog) {
         if (generateGeneratedAnnotation()) {
             String generated = "lombok.Generated";
             out.println("@%s", out.ref(generated));
         }
 
         //if (!scala) //TODO: uncomment when scala variable is protected instead of private
-        out.println("@%s({ \"all\", \"unchecked\", \"rawtypes\" })", out.ref("java.lang.SuppressWarnings"));
+        out.println("@%s({ \"all\", \"unchecked\", \"rawtypes\" })",
+                out.ref("java.lang.SuppressWarnings"));
     }
 }
