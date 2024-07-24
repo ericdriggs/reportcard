59be359e8db83510ab6d21cdc91e4d79f3890d20
eric.r.driggs <eric.r.driggs@disneystreaming.com>
2024-07-24 10:25:36 -0700

    bump version

--- reportcard-server/src/main/java/io/github/ericdriggs/reportcard/controller/browse/BrowseHtmlHelper.java
+++ reportcard-server/src/main/java/io/github/ericdriggs/reportcard/controller/browse/BrowseHtmlHelper.java
@@ -685,7 +685,7 @@ public class BrowseHtmlHelper {
                 &nbsp;&nbsp;&nbsp;
                 <a href="/swagger-ui/index.html">swagger</a>&nbsp;&nbsp;&nbsp;
                 <a href="https://github.com/ericdriggs/reportcard">source</a>&nbsp;&nbsp;&nbsp;
-                <span style="color:white">ver: 0.1.7</span>
+                <span style="color:white">ver: 0.1.8</span>
               </span>
             </header>
             <nav aria-label="breadcrumb">
