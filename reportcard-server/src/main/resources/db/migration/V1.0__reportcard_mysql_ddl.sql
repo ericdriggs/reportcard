acf1cdb15284cae40c9d23dc6afb1f4b79b1a00e
eric.r.driggs <eric.driggs@disney.com>
2026-01-27 13:50:33 -0800

    comment out drop schema

--- reportcard-server/src/main/resources/db/migration/V1.0__reportcard_mysql_ddl.sql
+++ reportcard-server/src/main/resources/db/migration/V1.0__reportcard_mysql_ddl.sql
@@ -13,7 +13,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema reportcard
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `reportcard` ;
+-- DROP SCHEMA IF EXISTS `reportcard` ;
 
 -- -----------------------------------------------------
 -- Schema reportcard
