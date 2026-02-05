9a48ba896db2027e50c419996ac49d6e0ca08135
eric.r.driggs <eric.driggs@disney.com>
2026-02-05 11:24:02 -0800

    wip

--- reportcard-server/src/main/resources/db/migration/V1.2__add_test_result_timing.sql
+++ reportcard-server/src/main/resources/db/migration/V1.2__add_test_result_timing.sql
@@ -2,5 +2,6 @@
 -- Run manually on deployed databases
 -- ALGORITHM=INPLACE, LOCK=NONE allows concurrent reads/writes during ALTER
 
-ALTER TABLE test_result ADD COLUMN start_time DATETIME NULL AFTER time, ALGORITHM=INPLACE, LOCK=NONE;
-ALTER TABLE test_result ADD COLUMN end_time DATETIME NULL AFTER start_time, ALGORITHM=INPLACE, LOCK=NONE;
+ALTER TABLE test_result
+  ADD COLUMN start_time DATETIME NULL AFTER time,
+  ADD COLUMN end_time DATETIME NULL AFTER start_time;
